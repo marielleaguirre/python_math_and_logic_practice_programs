19a81f382b34158ed49b65189306b49b9761de1e
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 18:12:49 +0800

    Added a pseudocode for all numbers except ending in zero

---
+++ print_all_numbers_except_ending_in_zero.py
@@ -0,0 +1,7 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+'''
+    loop through number from 0 to 100
+        check if number is not a multiple of 10 (if number divided by 10 has a remainder)
+            print all the numbers except ending in zero
+'''
