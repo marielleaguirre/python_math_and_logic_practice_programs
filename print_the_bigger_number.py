9b452097a6351d54c47a8bd6ce4b93d02d9d40b8
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 15:12:33 +0800

    Used if-else statement to determine the bigger number

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -10,3 +10,9 @@
 # ask the user to input first_number and second_number
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+# use if-else statement to determine the bigger number
+if first_number > second_number:
+    print("Bigger Number:", first_number)
+else:
+    print("Bigger Number:", second_number)
