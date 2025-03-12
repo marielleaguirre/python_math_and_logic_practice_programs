7a8effc65fc421781fbc010d201e1ccc891c2c0c
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:41:46 +0800

    Input the first_number and second_number

--- print_the_quotient_of_two_numbers_with_decimal.py
+++ print_the_quotient_of_two_numbers_with_decimal.py
@@ -4,3 +4,6 @@
     ask the user to input first_number and second_number
     print the quotient of the two numbers with decimal point
 '''
+# ask the user to input first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
