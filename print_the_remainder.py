e6a4e6b4fbde8fe33079bf628158e7e80fd82178
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 23:03:44 +0800

    Input the first_number and second_number

--- print_the_remainder.py
+++ print_the_remainder.py
@@ -5,3 +5,6 @@
     print the quotient of the first_number and second_number
     print the remainder of the quotient
 '''
+# ask the user to input first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
