e94c5a22c55be2796962729bbc2aa0c3ce94e8e4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:20:52 +0800

    Added the pseudocode for the product of two numbers

---
+++ print_the_product_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+'''
+    ask the user to imput the first_number and second_number
+    print the product of the two numbers
+'''
