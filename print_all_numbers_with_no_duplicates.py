ab7afa3e657ef4102d8a246a0461ad888ebeb96f
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 04:21:47 +0800

    Added the pseudocode that display all numbers that don't have duplicate

---
+++ print_all_numbers_with_no_duplicates.py
@@ -0,0 +1,7 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# initialize an empty list to store user input
+# ask user to input 10 numbers
+# find and display numbers that are not duplicated
+# if the count of the number is 1, it is unique
+# display the result
