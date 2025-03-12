ccfac93758eef43e67400c1b382f1794e1bb27c6
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:43:47 +0800

    Initialize an empty file for all numbers with duplicates

--- print_all_numbers_with_duplicates.py
+++ print_all_numbers_with_duplicates.py
@@ -1,6 +1,8 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # initialize an empty list to store user input
+list_numbers = []
+
 # ask user to input 10 numbers
 # find and display numbers that have duplicates
 # display the result
