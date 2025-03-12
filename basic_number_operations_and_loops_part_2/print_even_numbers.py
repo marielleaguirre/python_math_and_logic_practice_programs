e33836fe409f7df4f75a2eaca0aeec1ee1a91883
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 00:45:53 +0800

    Create a folder

---
+++ basic_number_operations_and_loops_part_2/print_even_numbers.py
@@ -0,0 +1,27 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+'''
+    initialize even_numbers container to 0
+    use for loop with range of 10
+        ask the user to input 10 numbers
+        if the number divided by 2 has 0 remainder then
+             increment even_numbers by 1
+    print the total of even numbers
+'''
+# initialize even_numbers container to 0
+even_numbers = 0
+
+# use for loop with range 10
+for i in range(10):
+
+    # ask the user to input 10 numbers
+    number = float(input(f"Number {i+1}: "))
+
+    # if the number input divided by 2 has 0 remainder
+    if number % 2 == 0:
+
+        # increment even_numbers by 1
+        even_numbers += 1
+
+# print the total of even_numbers
+print("Even Numbers: ", even_numbers)
