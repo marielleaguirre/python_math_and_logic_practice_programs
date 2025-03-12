91695e0f1ba70a00c38981ca3a3b6f85dc9ed6af
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 14:55:37 +0800

    Added the pseudocode for the bigger number

---
+++ print_the_bigger_number.py
@@ -0,0 +1,9 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+'''
+    ask the user to input first_number and second_number
+    if first_number is greater than second_number then
+        print first_number
+    else
+        print second_number
+'''
