46aac9e7c7f8d83d357282c2348a267e06cbaa15
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 06:32:08 +0800

    Added the pseudocode for calculating the average of the input numbers

---
+++ print_average.py
@@ -0,0 +1,8 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# initialize an empty list to store user input
+# continuously prompts the user to input a number
+# take user input
+# add the number to the list
+# stop asking for input when non-numeric value is entered
+# calculate and display the average of the numbers that were entered
