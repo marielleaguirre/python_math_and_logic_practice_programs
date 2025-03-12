6efa278081b716a1cf36c57c26a58f0cadfa35ed
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 18:16:45 +0800

    Added the code that print all number from 0 to 100 except numbers ending in zero

--- print_all_numbers_except_ending_in_zero.py
+++ print_all_numbers_except_ending_in_zero.py
@@ -5,3 +5,11 @@
         check if number is not a multiple of 10 (if number divided by 10 has a remainder)
             print all the numbers except ending in zero
 '''
+# use for loop with range of 101
+for number in range(101):
+
+    # check if number is not multple of 10
+    if number % 10 != 0:
+
+        # print all the numbers that is not ending in zero
+        print(number)
