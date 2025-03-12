8344458d508258a3f590897fbde02ed8e7f7067c
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 23:58:31 +0800

    Added a pseudocode for all odd numbers from 0 to 100 using while loop

---
+++ print_all_odd_numbers_from_0_to_100_using_while_loop.py
@@ -0,0 +1,9 @@
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+'''
+    initialize odd_number container to 0
+    use while loop until odd_number is less than or equal to 100
+        check if odd_number divided by 2 has a remainder
+            if the condition is true, print the odd_number
+        increment odd_number by 1 to check the next number 
+'''
