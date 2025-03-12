2baeeae7c3a2ac38659a544e8db9c4f4b422a369
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 04:26:19 +0800

    Ask user to input 10 numbers

--- print_all_numbers_with_no_duplicates.py
+++ print_all_numbers_with_no_duplicates.py
@@ -4,6 +4,10 @@
 list_numbers = []
 
 # ask user to input 10 numbers
+for i in range(10):
+    number = float(input(f"Number {i+1}: "))
+    list_numbers.append(number)
+    
 # find and display numbers that are not duplicated
 # if the count of the number is 1, it is unique
 # display the result
