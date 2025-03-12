38e06c0d4df3d2a46ffdca3d2ba9bf38ad623acc
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 15:50:35 +0800

    Input the first_number and second_number

--- print_if_two_numbers_are_equal.py
+++ print_if_two_numbers_are_equal.py
@@ -5,3 +5,6 @@
     if first_number is equals to second_number then
         print Equal
 '''
+# ask the user to input first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
