2710091f4d804cae6088a717f3ecbe8ed38a964e
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:10:07 +0800

    Input the first_number and second_number

--- print_the_sum_of_two_numbers.py
+++ print_the_sum_of_two_numbers.py
@@ -4,3 +4,6 @@
     ask user to input the first_number and second_number
     print the sum of the two numbers
 '''
+# ask user to input the first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
