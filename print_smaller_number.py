73ee2a8a9ee1a9c2b2abe23976339a4b97b07be8
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:21:09 +0800

    Input the first_number and second_number

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -4,3 +4,6 @@
     ask user to input the first_number and second_number
     use if-else statement to determine the smaller number
 '''
+# ask the user to input two numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
