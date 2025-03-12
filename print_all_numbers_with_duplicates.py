390098bdacc36bf2e2242f3d2f218763c38eb790
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:46:32 +0800

    Ask user to input 10 numbers. Uses for loop with range of 10 and the append() function

--- print_all_numbers_with_duplicates.py
+++ print_all_numbers_with_duplicates.py
@@ -4,5 +4,8 @@
 list_numbers = []
 
 # ask user to input 10 numbers
+for i in range(10):
+    number = float(input(f"Number {i+1}: "))
+    list_numbers.append(number)
 # find and display numbers that have duplicates
 # display the result
