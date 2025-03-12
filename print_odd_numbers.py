e0cf604b992d5fba9d409ec607ea3142882e5b16
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 17:39:23 +0800

    Added the pseudocode for odd numbers

---
+++ print_odd_numbers.py
@@ -0,0 +1,10 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+'''
+    initialize the odd_numbers container to 0
+    use for loop with range 10
+        ask user to input 10 numbers
+        use if statement to determine whether the number is odd or not
+            add 1 to odd_numbers if the number input is an odd number
+    print how many odd numbers are input
+'''
