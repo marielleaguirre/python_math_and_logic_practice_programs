ec17719589283d575edd56969918ef6c80999863
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 23:16:33 +0800

    Added the pseudocode for the difference of first number and the remaining numbers

---
+++ print_the_difference_of_first_number_and_the_remaining_numbers.py
@@ -0,0 +1,12 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+'''
+    initialize total_difference conatainer to 0
+    use for loop with range 10
+        ask the user to input 10 numbers
+        if the iteration is equals to 0 then
+            add the number input to the total_difference
+        else
+            subtract the number input to the total_difference
+    print the total_difference
+'''
