8e2d5351fadd30e50bf9efc20b556a2dbc94c288
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 18:02:02 +0800

    Used for loop with range 101

--- print_all_even_number_using_for_loop.py
+++ print_all_even_number_using_for_loop.py
@@ -5,3 +5,5 @@
         in each iteration check if the number is even (if number divided by 2 has no remainder)
             print the number
 '''
+# use for loop with the range of 101
+for number in range(101):
