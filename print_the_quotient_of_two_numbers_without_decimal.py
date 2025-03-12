462c90a3d3bfedfadfc4a63538d0c9b901b108e7
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:52:05 +0800

    Added the pseudocode for the quotient of the numbers without decimal point

---
+++ print_the_quotient_of_two_numbers_without_decimal.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+'''
+    ask user to input the first_number and second_number
+    print the quotient of the two numbers without decimal point (using int to convert input number to integer)
+'''
