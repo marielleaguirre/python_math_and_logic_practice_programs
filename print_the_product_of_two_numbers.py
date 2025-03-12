89943cb2b14a5f48bcc3f682cffeed62f7c80b0c
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:23:24 +0800

    Input the first_number and second_number

--- print_the_product_of_two_numbers.py
+++ print_the_product_of_two_numbers.py
@@ -1,6 +1,9 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 '''
-    ask the user to imput the first_number and second_number
+    ask the user to input the first_number and second_number
     print the product of the two numbers
 '''
+# ask the user to input the first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
