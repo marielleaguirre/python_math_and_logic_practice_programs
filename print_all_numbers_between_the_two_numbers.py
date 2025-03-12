eac0b7bdf7cb1e8a1e02adb50398afcc51afde17
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 00:26:32 +0800

    Added a pseudocode that determine all numbers in between the two numbers input

---
+++ print_all_numbers_between_the_two_numbers.py
@@ -0,0 +1,9 @@
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+'''
+    ask the user to input the first_number and second_number
+    check if first_number is greater than second_number
+        swap the values of first_number and second_number (to ensure proper order)
+    loop through numbers from first_number plus 1 to second_number
+        print each number between the range
+'''
