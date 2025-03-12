f8877d7c8afe16c6b3310b6432de895d52d39554
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 17:22:14 +0800

    Added the pseudocode for the sum of all numbers

---
+++ print_the_sum_of_all_numbers.py
@@ -0,0 +1,9 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+'''
+    initialize total_sum to 0
+    use for loop with a range of 10
+        ask the user to input 10 numbers
+        add number to total_sum
+    print the sum of all numbers
+'''
