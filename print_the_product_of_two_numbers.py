9385c26e474541aadc9280bdeb578ef594e9fda4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:26:27 +0800

    Added the code that print the product of the two numbers

--- print_the_product_of_two_numbers.py
+++ print_the_product_of_two_numbers.py
@@ -7,3 +7,6 @@
 # ask the user to input the first_number and second_number
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+# print the product of the two numbers
+print("The product of", first_number, "and", second_number, "is", first_number * second_number)
