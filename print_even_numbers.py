18505706a6186aeeba5d4f42353002b69942f9fd
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 23:38:00 +0800

    Added a pseudocode for even numbers

---
+++ print_even_numbers.py
@@ -0,0 +1,10 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+'''
+    initialize even_numbers container to 0
+    use for loop with range of 10
+        ask the user to input 10 numbers
+        if the number divided by 2 has 0 remainder then
+             increment even_numbers by 1
+    print the total of even numbers
+'''
