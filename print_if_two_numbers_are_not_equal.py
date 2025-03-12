15ea45c61fd8c0bfab9bd1a33a2584132cc0ec3c
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:32:41 +0800

    Added the pseudocode for determining if two numbers are not equal

---
+++ print_if_two_numbers_are_not_equal.py
@@ -0,0 +1,6 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+'''
+    ask user to input the first_number and second_number
+    use if statement to determine if the two numbers are not equal
+'''
