f39e795922ae4891398b4b5be4b2ae1e4397cc6a
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:17:58 +0800

    Added the pseudocode for displaying the numbers from lowest to highest using sort() function

---
+++ print_ascending_order_using_sort.py
@@ -0,0 +1,10 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display the number from lowest to highest. Clue: sort() function
+
+# initialize an empty list to store user input
+# continuously prompts the user to input a number
+# take user input
+# add the number to the list
+# stop asking for input when non-numeric value is entered
+# sort and display the list of numbers if the list is not empty
+# sort the list in ascending order
