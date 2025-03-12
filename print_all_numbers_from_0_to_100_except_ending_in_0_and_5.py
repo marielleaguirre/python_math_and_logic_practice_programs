53b70355551c34e207b6744f4e1ecdaf513a8858
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 00:14:12 +0800

    Added a pseudocode for all numbers from 0 to 100 except ending in 0 or 5

---
+++ print_all_numbers_from_0_to_100_except_ending_in_0_and_5.py
@@ -0,0 +1,7 @@
+# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+'''
+    loop through numbers from 0 to 100
+        check if number is not a multiple of 5
+            print number
+'''
