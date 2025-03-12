d96b67623740f43d40d1ce65a78bce5483d1ac39
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:39:23 +0800

    Used if statement to determine if the two numbers are not equal

--- print_if_two_numbers_are_not_equal.py
+++ print_if_two_numbers_are_not_equal.py
@@ -7,3 +7,7 @@
 # ask the user to input first_number and second_number
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+# use if statement to determine if the two numbers are not equal and print Not Equal
+if first_number != second_number:
+    print("The numbers are Not Equal.")
