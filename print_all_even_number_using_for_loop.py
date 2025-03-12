626fc2ea852626bef7d75a14ba563cf8a8a9adc4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 18:00:31 +0800

    Added the pseudocode for even numbers

---
+++ print_all_even_number_using_for_loop.py
@@ -0,0 +1,7 @@
+# Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+'''
+    loop through numbers from 0 to 100
+        in each iteration check if the number is even (if number divided by 2 has no remainder)
+            print the number
+'''
