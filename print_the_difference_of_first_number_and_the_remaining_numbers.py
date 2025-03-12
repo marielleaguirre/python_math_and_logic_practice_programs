c2f44299818a8d8f44ee9a688994051d5edb5333
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 23:18:53 +0800

    Initialize total_difference container to 0

--- print_the_difference_of_first_number_and_the_remaining_numbers.py
+++ print_the_difference_of_first_number_and_the_remaining_numbers.py
@@ -1,7 +1,7 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 '''
-    initialize total_difference conatainer to 0
+    initialize total_difference container to 0
     use for loop with range 10
         ask the user to input 10 numbers
         if the iteration is equals to 0 then
@@ -10,3 +10,5 @@
             subtract the number input to the total_difference
     print the total_difference
 '''
+# initialize total_difference container to 0
+total_difference = 0
