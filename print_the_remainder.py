ad7c3d34383115c33ff0ab209e2cc09c98fd47a1
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 23:02:49 +0800

    Added the pseudocode for the remainder

---
+++ print_the_remainder.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+'''
+    ask the user to input the first_number and second_number
+    print the quotient of the first_number and second_number
+    print the remainder of the quotient
+'''
