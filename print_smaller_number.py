2e99578c6186f8c6ab8fc8891e8d13fe3e677886
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:26:30 +0800

    Changes in pseudocode and used if-else statement to determine the smaller number

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -2,8 +2,17 @@
 
 '''
     ask user to input the first_number and second_number
-    use if-else statement to determine the smaller number
+    if first_number is less than second_number then
+        print first_number
+    else
+        print second_number
 '''
 # ask the user to input two numbers
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+# use if-else statement to determine the smaller number
+if first_number < second_number:
+    print("Smaller Number:", first_number)
+else:
+    print("Smaller Number:", second_number)
