c13293ece6e01c66e0d151fa706dea2ca6d8ec67
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:05:46 +0800

    Added the pseudocode for the lowest number

---
+++ print_lowest_number.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# initialize an empty list to store user input
+# continuously prompts the user to input a number
+# take user input
+# add the number to the list
+# stop asking for input when non-numeric value is entered
+# display the lowest umber if the ist is not empty
