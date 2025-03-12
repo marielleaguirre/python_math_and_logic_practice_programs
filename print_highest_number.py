1e57b1fbaa956ebc12c5f86f050e40486c53e5ee
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 06:09:47 +0800

    Initialize an empty list highest_number

--- print_highest_number.py
+++ print_highest_number.py
@@ -1,6 +1,8 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 # initialize an empty list to store user input
+highest_number = []
+
 # continuously prompts the user to input a number
 # take user input
 # add the number to the list
