c9dfc73e07607d1f514e94eec0ce814525c7f909
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 15:48:05 +0800

    Added the pseudocode for equal numbers

---
+++ print_if_two_numbers_are_equal.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+'''
+    ask the user to input first_number and second_number
+    if first_number is equals to second_number then
+        print Equal
+'''
