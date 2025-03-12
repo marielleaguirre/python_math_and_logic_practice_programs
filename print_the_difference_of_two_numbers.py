94a54dfc2fd7b77047b684bb28ff49e6884d0dc4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:46:51 +0800

    Print the difference of the two numbers

--- print_the_difference_of_two_numbers.py
+++ print_the_difference_of_two_numbers.py
@@ -7,3 +7,6 @@
 # ask the user to input first_number and second_number
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+# print the difference of the two numbers
+print("The difference of", first_number, "and", second_number, "is", first_number - second_number)
