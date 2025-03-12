e6a822a71c34eede7ef56eef0f59105bf0fdcf64
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:42:21 +0800

    Added the pseudocode for all numbers with duplicate

---
+++ print_all_numbers_with_duplicates.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# initialize an empty list to store user input
+# ask user to input 10 numbers
+# find and display numbers that have duplicates
+# display the result
