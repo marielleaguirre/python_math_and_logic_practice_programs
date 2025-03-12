8c1741aa405584628a24a1b251ec0c5b360c523d
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 04:56:31 +0800

    Added the pseudocode for displaying unique or duplicated input

---
+++ print_unique_or_duplicate_input.py
@@ -0,0 +1,13 @@
+# Prog03: Create a program that ask user to input a number
+# continue asking until the user input is invalid. 
+# Display "Unique" after input when the inputted number don't have duplicate. 
+# Display "Duplicate" after input when the inputted number have duplicate.
+
+# initialize an empty list to store user input
+# continuously prompts the user to input a number
+# take user input
+# check if the number alreasy exists in the list
+# display "Duplicate" if the number already exists
+# display "Unique" if it is a new number
+# add the number to the list
+# stop asking for input when non-numeric value is entered 
