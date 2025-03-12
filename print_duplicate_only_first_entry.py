8c3b9d3bf6c9ebe88d2e693221859d857c198169
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 04:41:11 +0800

    Ask user to input 10 numbers. Uses not in and append() function to only store the first entry of a duplicate

--- print_duplicate_only_first_entry.py
+++ print_duplicate_only_first_entry.py
@@ -4,6 +4,9 @@
 list_numbers = []
 
 # ask user to input 10 numbers
-# take input as integer
-# only store the first entry
+for i in range(10):
+    number = float(input(f"Number {i+1}: "))  # take input as integer
+    if number not in list_numbers:  # only store the first entry
+        list_numbers.append(number)
+        
 # display the result
