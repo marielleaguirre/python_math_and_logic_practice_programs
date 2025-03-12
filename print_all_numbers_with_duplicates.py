76d32191888574d68d20db8a53bdd377c95cb7f0
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:52:00 +0800

    Find and display numbers with duplicates. Uses for loop, count() function, and append() function

--- print_all_numbers_with_duplicates.py
+++ print_all_numbers_with_duplicates.py
@@ -7,5 +7,11 @@ list_numbers = []
 for i in range(10):
     number = float(input(f"Number {i+1}: "))
     list_numbers.append(number)
+
 # find and display numbers that have duplicates
+duplicate_numbers = []
+for number in list_numbers:
+    if list_numbers.count(number) > 1 and number not in duplicate_numbers:
+        duplicate_numbers.append(number)
+
 # display the result
