410e705e08ebd16a51fcfbcf31d9e37e8ea1f02b
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 04:35:23 +0800

    Added a pseudocode for all numbers but for dupliates first entry only

---
+++ print_duplicate_only_first_entry.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# initialize an empty list to store user input
+# ask user to input 10 numbers
+# take input as integer
+# only store the first entry
+# display the result
