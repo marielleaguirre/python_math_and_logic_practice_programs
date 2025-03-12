4f0cf3ec34180b7c0488afe70abc1f5e38cc5240
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:56:51 +0800

    Added the pseudocode for the most duplicate numbers

---
+++ print_most_duplicate_numbers.py
@@ -0,0 +1,10 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display the number with the most number of duplicate.
+
+# initialize an empty list to store user input
+# continuously prompts the user to input a number
+# take user input
+# add the number to the list
+# stop asking for input when non-numeric value is entered
+# find the number with the most duplicates
+# find the most repeated number
