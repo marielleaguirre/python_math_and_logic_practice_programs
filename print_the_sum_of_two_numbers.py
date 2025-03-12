853a9f62c06bb6c0eff9dccefa39c6b33dd2fa28
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:05:10 +0800

    Added the pseudocode for the sum of two numbers

---
+++ print_the_sum_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+'''
+    ask user to input the first_number and second_number
+    print the sum of the two numbers
+'''
