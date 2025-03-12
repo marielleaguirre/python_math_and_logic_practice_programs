9463360134db7efc6d16d9f1be1974f300711bd9
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:02:30 +0800

    Added the main code for unique or duplicated input. Uses while loop and try and except

--- print_unique_or_duplicate_input.py
+++ print_unique_or_duplicate_input.py
@@ -4,10 +4,22 @@
 # Display "Duplicate" after input when the inputted number have duplicate.
 
 # initialize an empty list to store user input
+list_numbers = []
+
 # continuously prompts the user to input a number
-# take user input
-# check if the number alreasy exists in the list
-# display "Duplicate" if the number already exists
-# display "Unique" if it is a new number
-# add the number to the list
-# stop asking for input when non-numeric value is entered 
+while True:
+    try:
+        # take user input
+        number = float(input("Enter a number: "))
+        
+        # check if the number alreasy exists in the list
+        if number in list_numbers:
+            print(number, "is a DUPLICATE.")  # display "Duplicate" if the number already exists
+        else:
+            print(number, "is UNIQUE.")  # display "Unique" if it is a new number
+            list_numbers.append(number)  # add the number to the list
+            
+    except ValueError:
+        # stop asking for input when non-numeric value is entered 
+        print("Invalid input detected. Program stopped.")
+        break
