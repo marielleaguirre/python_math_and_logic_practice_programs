92d17267d8826fd5f15e8e58942040943f611a84
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 16:37:06 +0800

    Added the pseudocode for the quotient of the two numbers with decimal

---
+++ print_the_quotient_of_two_numbers_with_decimal.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+'''
+    ask the user to input first_number and second_number
+    print the quotient of the two numbers with decimal point
+'''
