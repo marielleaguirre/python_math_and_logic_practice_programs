90d18ea3ed4150c67d87abbef57bb0448bdd0111
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 17:07:49 +0800

    Added the code for the exponent

--- print_exponent.py
+++ print_exponent.py
@@ -7,3 +7,6 @@
 # ask the user to input first_number and second_number
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+# print the exponent
+print(first_number, "raised to", second_number, "is", first_number ** second_number)
