d87799cbc9b46fac339590486aeb37a245158243
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:33:49 +0800

    Input the first_number and second_number

--- print_if_two_numbers_are_not_equal.py
+++ print_if_two_numbers_are_not_equal.py
@@ -4,3 +4,6 @@
     ask user to input the first_number and second_number
     use if statement to determine if the two numbers are not equal
 '''
+# ask the user to input first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
