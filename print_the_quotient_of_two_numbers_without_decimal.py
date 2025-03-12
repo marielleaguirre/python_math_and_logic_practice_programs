6570231fe3568def20cc5374c7fa0cdac26a8726
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:53:13 +0800

    Input the first_number and second_number

--- print_the_quotient_of_two_numbers_without_decimal.py
+++ print_the_quotient_of_two_numbers_without_decimal.py
@@ -4,3 +4,6 @@
     ask user to input the first_number and second_number
     print the quotient of the two numbers without decimal point (using int to convert input number to integer)
 '''
+# ask the user to input first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
