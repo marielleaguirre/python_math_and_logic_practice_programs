9d1a7745d47120ceb77ee839ccc1498bad0c9a7b
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:18:34 +0800

    Added the pseudocode for smaller number

---
+++ print_smaller_number.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+'''
+    ask user to input the first_number and second_number
+    use if-else statement to determine the smaller number
+'''
