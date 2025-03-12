32b96e6b38c19aa6c990d21b09cda40421258785
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 06:08:45 +0800

    Added the pseudocode for displaying the highest number

---
+++ print_highest_number.py
@@ -0,0 +1,8 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+# initialize an empty list to store user input
+# continuously prompts the user to input a number
+# take user input
+# add the number to the list
+# stop asking for input when non-numeric value is entered
+# display the highest number if the list is not empty
