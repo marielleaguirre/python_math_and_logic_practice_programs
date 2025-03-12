635d4e3303897ff56ce8d035de41aedec5417924
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:24:50 +0800

    Added the main code for sorting in ascending order. Uses while loop, try and except, if-else statement, and sort() function

--- print_ascending_order_using_sort.py
+++ print_ascending_order_using_sort.py
@@ -2,9 +2,22 @@
 # Display the number from lowest to highest. Clue: sort() function
 
 # initialize an empty list to store user input
+list_numbers = []
+
 # continuously prompts the user to input a number
-# take user input
-# add the number to the list
-# stop asking for input when non-numeric value is entered
+while True:
+    try:
+        # take user input
+        number = float(input("Enter a number: "))
+        list_numbers.append(number)  # add the number to the list
+    except ValueError:
+        print("Invalid Input. Now sorting in ascending order...")
+        # stop asking for input when non-numeric value is entered
+        break
+
 # sort and display the list of numbers if the list is not empty
-# sort the list in ascending order
+if list_numbers:
+    list_numbers.sort()  # sort the list in ascending order
+    print("Number from lowest to highest", list_numbers)
+else: 
+    print("No numbers were entered.")
