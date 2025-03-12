fc14e1e19071e1709a4034a7d40a30cd911e79a0
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 15:05:56 +0800

    Input the first_number and second_number

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -7,3 +7,6 @@
     else
         print second_number
 '''
+# ask the user to input first_number and second_number
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
