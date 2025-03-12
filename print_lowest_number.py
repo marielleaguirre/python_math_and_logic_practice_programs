bf167e871166eaffd412286d6ebbe92fef634f76
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-13 05:09:39 +0800

    Initialized empty list, ask user to enter 10 input. Uses while loop and try and except

--- print_lowest_number.py
+++ print_lowest_number.py
@@ -1,8 +1,16 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 # initialize an empty list to store user input
+lowest_number = []
+
 # continuously prompts the user to input a number
-# take user input
-# add the number to the list
-# stop asking for input when non-numeric value is entered
+while True:
+    try:
+        # take user input
+        number = float(input("Enter a number: "))
+        lowest_number.append(number)  # add the number to the list
+    except ValueError:
+        # stop asking for input when non-numeric value is entered
+        break
+    
 # display the lowest umber if the ist is not empty
