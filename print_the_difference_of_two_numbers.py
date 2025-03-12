33d0d02a644c6d208a2f12cac0bc2f405c73957e
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-12 22:43:48 +0800

    Added the pseudocode for the difference of two numbers

---
+++ print_the_difference_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+'''
+    ask the user to input the first_number and second_number
+    print the difference of the two numbers
+'''
