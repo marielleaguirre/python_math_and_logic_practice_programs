612cc39a3547e9db60b6b673869b1fa83c7cc9ea
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 15:13:10 +0800

    Used title() and replace() method to convert input to proper casing then remove the spaces

--- pascal_case_full_name.py
+++ pascal_case_full_name.py
@@ -8,4 +8,6 @@
 full_name = input("Enter your full name in incorrect casing: ")
 
 # use title() method to convert input to proper casing then replace() method to remove the spaces
+pascal_case_name = full_name.title().replace(" ", "")
+
 # print the full name in pascal case
