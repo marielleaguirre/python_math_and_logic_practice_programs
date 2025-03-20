330904398764d117faf72739360eadb72082c38a
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 13:45:50 +0800

    Added a pseudocode for removing leading spaces

---
+++ remove_leading_spaces.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname with several space characters at the beginning. 
+# Print the input without the spaces in the beginning.
+# Example:
+# Input:         Juan Dela Cruz
+# Output: Juan Dela Cruz
+
+# ask user to input their full name with several spaces in the beginning
+# use lstrip() method to remove left padded spaces
+# print the full name without the spaces at the beginning
