55d00516aa5dec797b628ac8ae7978cdb3c16ead
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:15:32 +0800

    Ask user to input their full name

--- uppercase_full_name.py
+++ uppercase_full_name.py
@@ -5,5 +5,7 @@
 # Output: JUAN DELA CRUZ
 
 # ask the user to input their full name
+full_name = input("Enter your full name: ")
+
 # use upper() method to turn all the characters of the full name to uppercase
 # print the full name in uppercase
