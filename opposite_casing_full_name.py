912d5ed760b105f3581681f846b64385d6e47401
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:38:03 +0800

    Added the pseudocode for the reversing of casing of the characters in user input

---
+++ opposite_casing_full_name.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname in incorrect casing. 
+# Print each character of the input in reverse casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuaN deLA cRuz
+
+# ask user to input their full name in incorrect casing
+# use swapcase() method to convert all characters in opposite casing
+# print the full name in reverse casing
