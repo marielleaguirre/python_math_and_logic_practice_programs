daba504c625dcbd7f713080e128ef75d9a3a4493
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 15:20:29 +0800

    Ask the user to input their full name in incorrect casing

--- snake_case_full_name.py
+++ snake_case_full_name.py
@@ -5,5 +5,7 @@
 # Output: juan_dela_cruz
 
 # ask the user to input their full name in incorrect casing
+full_name = input("Enter your full name in incorrect casing: ")
+
 # use lower() method to convert the string to lowercase then replace() method to replace spaces with underscores
 # print the full name in snake case
