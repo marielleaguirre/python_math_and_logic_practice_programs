9782f57ba21f290d50dd2ef140440ee0d259679a
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 15:09:18 +0800

    Added a pseudocode for pascal casing of the full name

---
+++ pascal_case_full_name.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname in incorrect casing. 
+# Print the input in pascal case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuanDelaCruz
+
+# ask th user to input their name in incorrect casing
+# use title() method to convert input to proper casing then replace() method to remove the spaces
+# print the full name in pascal case
