b92548b7ad7b3a0d2a920c446675c96ef336fb89
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:58:55 +0800

    Added a pseudocode for counting the number of characters in the user input

---
+++ characters_in_full_name_counter.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname. 
+# Print the number of characters in the input.
+# Example:
+# Input: Juan Dela Cruz
+# Output: 14
+
+# ask the user to input their full name
+# use len() method to count the number of characters in the user input
+# print the number of characters in the full name
