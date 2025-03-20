6a136c442064a2e0001c6da38b6c378f3843f225
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:14:14 +0800

    Added a pseudocode for uppercasing of the full name

---
+++ uppercase_full_name.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname.
+# Print the input in all capital letter.
+# Example:
+# Input: Juan Dela Cruz
+# Output: JUAN DELA CRUZ
+
+# ask the user to input their full name
+# use upper() method to turn all the characters of the full name to uppercase
+# print the full name in uppercase
