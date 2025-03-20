6e9af42d5f2cffec935c5b6d297b3ec410d4aa55
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:30:29 +0800

    Added a pseudocode for proper casing of the user input

---
+++ titlecase_full_name.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname in incorrect casing. 
+# Print the input in proper casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: Juan Dela Cruz
+
+# ask the user to input their full name in incorrect casing
+# use title() method to convert input to title case
+# print the full name in proper casing
