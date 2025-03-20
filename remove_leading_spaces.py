5f3fc80bd62e8acc61f6a88273d757cc55a16ee6
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 13:52:05 +0800

    Print the user's input with no leading spaces

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -11,3 +11,4 @@ full_name = input("Enter your full name (with several spaces at the beginning): 
 remove_leading_spaces = full_name.lstrip()
 
 # print the full name without the spaces at the beginning
+print("Full name (no leading spaces):", remove_leading_spaces)
