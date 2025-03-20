4265a9bc97fb7cf2a561e6a41f451fccd0814c2d
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 15:18:46 +0800

    Added a pseudocode for snake casing of the full name

---
+++ snake_case_full_name.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname in incorrect casing. 
+# Print the input in snake case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: juan_dela_cruz
+
+# ask the user to input their full name in incorrect casing
+# use lower() method to convert the string to lowercase then replace() method to replace spaces with underscores
+# print the full name in snake case
