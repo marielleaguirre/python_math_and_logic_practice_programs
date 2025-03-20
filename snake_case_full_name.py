35618713a01914533b5ac605e2ba6aeb3495eb02
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 15:22:48 +0800

    Used lower() and replace() method to convert string to lowercase then replace spaces with underscores

--- snake_case_full_name.py
+++ snake_case_full_name.py
@@ -8,4 +8,6 @@
 full_name = input("Enter your full name in incorrect casing: ")
 
 # use lower() method to convert the string to lowercase then replace() method to replace spaces with underscores
+snake_case_name = full_name.lower().replace(" ", "_")
+
 # print the full name in snake case
