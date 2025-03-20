0db5754ba214b5093fa911416a28a3c58b7a991e
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:22:00 +0800

    Added a pseudocode for lowercasing of all characters in the user input

---
+++ lowercase_full_name.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input their fullname. 
+# Print the input in all lower case.
+# Example:
+# Input: Juan Dela Cruz
+# Output: juan dela cruz
+
+# ask user to input their full name
+# use lower() method to turn all characters to lowercase
+# print the full name in lowercase
