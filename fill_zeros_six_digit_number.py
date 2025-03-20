9ac00e6a32d333459512009e6c2d562cbe01c017
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:04:12 +0800

    Added the pseudocode for six digit format number + filing zeros in the beginning

---
+++ fill_zeros_six_digit_number.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input a number (0-1000). 
+# Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+# Example:
+# Input: 143
+# Output: 000143
+
+# ask user to input a number from 0 to 1000
+# use zfill() method to fill the beginning with zeroes to make a 6-digit format number
+# print the input in 6-digit format
