bc6552de76d5c9f7d79bfb7b9aa14816fa3d2ef7
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:39:32 +0800

    Ask user to input their full name in incorrect casing

--- opposite_casing_full_name.py
+++ opposite_casing_full_name.py
@@ -5,5 +5,7 @@
 # Output: JuaN deLA cRuz
 
 # ask user to input their full name in incorrect casing
+full_name = input("Enter your full name in incorrect casing: ")
+
 # use swapcase() method to convert all characters in opposite casing
 # print the full name in reverse casing
