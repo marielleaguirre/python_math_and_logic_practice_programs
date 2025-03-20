ce3e39b31967f4ddf184ee933e364a526e9bb572
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-20 14:48:13 +0800

    Added a pseudocode for the counting of the numbers of all the words in the statement input

---
+++ words_in_statement_counter.py
@@ -0,0 +1,9 @@
+# Create a program that ask the user to input a complete statement. 
+# Print the number of words in the input.
+# Example:
+# Input: We will weather the weather whatever the weather whether we like it or not
+# Output: 14
+
+# ask user to input a complete statement
+# use split() method to split the words in the statement then len() method for counting the number of words
+# print the number of words in the statement
